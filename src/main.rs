f635fe4d17b5cee217ac231e75e5d7577effe985
soh335 <sugarbabe335@gmail.com>
2017-08-24 16:19:01 +0900

    chapter 7 done

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,20 @@ use hitable_list::*;
 use hitable::*;
 use camera::*;
 
+fn random_in_unit_sphere() -> Vec3 {
+    let mut rng = rand::thread_rng();
+    loop {
+        let p = Vec3(rng.gen_range(-1.0,1.0),rng.gen_range(-1.0,1.0),rng.gen_range(-1.0,1.0));
+        if p.squard_length() < 1.0 {
+            return p
+        }
+    }
+}
+
 fn color<T: Hitable>(r: &Ray, world: &T) -> Vec3 {
-    if let Some(rec) = world.hit(r, 0.0, f32::MAX) {
-        0.5 * (Vec3::unit_vector(rec.normal)+Vec3(1.0,1.0,1.0))
+    if let Some(rec) = world.hit(r, 0.000001, f32::MAX) {
+        let target = rec.p + Vec3::unit_vector(rec.normal) + random_in_unit_sphere();
+        0.5 * color( &Ray(rec.p, target - rec.p), world )
     } else {
         let unit_direction = Vec3::unit_vector(r.direction());
         let t = 0.5 * (unit_direction.y() + 1.0);
@@ -46,6 +57,7 @@ fn main() {
                 col = col + color(&r, &world)
             }
             let col = col / ns as f32;
+            let col = Vec3(col.x().sqrt(), col.y().sqrt(), col.z().sqrt());
             let ir = (255.59*col.x()) as u8;
             let ig = (255.59*col.y()) as u8;
             let ib = (255.59*col.z()) as u8;
