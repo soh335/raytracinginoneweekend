830a9537f210a86d8a4d0d7e7f3b4b7e65a22780
soh335 <sugarbabe335@gmail.com>
2017-08-24 15:01:09 +0900

    chapter 06 done

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 extern crate raytracinginoneweekend;
+extern crate rand;
+
+use rand::Rng;
 use std::f32;
+
 use raytracinginoneweekend::*;
 use vec3::*;
 use ray::*;
 use sphere::*;
 use hitable_list::*;
 use hitable::*;
+use camera::*;
 
 fn color<T: Hitable>(r: &Ray, world: &T) -> Vec3 {
     if let Some(rec) = world.hit(r, 0.0, f32::MAX) {
@@ -20,6 +25,7 @@ fn color<T: Hitable>(r: &Ray, world: &T) -> Vec3 {
 fn main() {
     let nx = 200;
     let ny = 100;
+    let ns = 100;
 
     print!("P3\n{} {}\n255\n", nx, ny);
 
@@ -27,17 +33,19 @@ fn main() {
     world.push(Box::new(Sphere{center: Vec3(0.0,0.0,-1.0), radius: 0.5}));
     world.push(Box::new(Sphere{center: Vec3(0.0,-100.5,-1.0), radius: 100.0}));
 
-    let lower_left_corner = Vec3(-2.0,-1.0,-1.0);
-    let horizontal = Vec3(4.0,0.0,0.0);
-    let vertical = Vec3(0.0,2.0,0.0);
-    let origin = Vec3(0.0,0.0,0.0);
+    let camera = Camera::new();
+    let mut rng = rand::thread_rng();
 
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let u = i as f32 / nx as f32;
-            let v = j as f32 / ny as f32;
-            let r = Ray(origin, lower_left_corner + u * horizontal + v * vertical);
-            let col = color(&r, &world);
+            let mut col = Vec3(0.0,0.0,0.0);
+            for _ in 0..ns {
+                let u = (i as f32 + rng.gen_range(0.0,1.0)) / nx as f32;
+                let v = (j as f32 + rng.gen_range(0.0,1.0)) / ny as f32;
+                let r = camera.get_ray(u, v);
+                col = col + color(&r, &world)
+            }
+            let col = col / ns as f32;
             let ir = (255.59*col.x()) as u8;
             let ig = (255.59*col.y()) as u8;
             let ib = (255.59*col.z()) as u8;
